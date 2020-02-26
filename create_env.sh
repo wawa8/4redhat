5cec525a135b684d084803e046c59d7d1c2b3334
root <root@bastion.46f9.example.opentlc.com>
2020-02-26 15:54:06 +0000

    Ansible 4.3-4 changes

---
+++ create_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+TOWER_GUID=46f9
+OSP_GUID=3ace
+OPENTLC_LOGIN=maciej.wawrzyniak-atos.net
+OPENTLC_PASSWORD=8wawa888W
+GITHUB_REPO=https://github.com/wawa8/ansible_advance_homework
+JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
+REGION=us-east-1
+RH_MAIL_ID=maciej.wawrzyniak
